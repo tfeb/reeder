934c8fb7e0634bb22279d663b9ab3bf908b90d78
Tim Bradshaw <tfb@cley.com>
2024-06-25 12:36:12 +0100

    Example token reeder for rational decimals
    And the symbol parser tries to deal with packages
    
    Both of these have very little testing

---
+++ examples/rational-decimal-reeder.lisp
@@ -0,0 +1,75 @@
+;;;; Read decimal numbers as rationals
+;;;
+
+(org.tfeb.tools.require-module:needs
+ ((:org.tfeb.hax.collecting
+   :org.tfeb.toys.reeder)
+  :compile t))
+
+(defpackage :org.tfeb.play
+  (:use
+   :cl
+   :org.tfeb.toys.reeder
+   :org.tfeb.hax.collecting))
+
+(in-package :org.tfeb.play)
+
+(defvar *rational-decimal-reedtable* (copy-reedtable))
+
+(define-token-parser (rational-decimal 1 :reedtable *rational-decimal-reedtable*)
+    ((:sequence
+      :start-anchor
+      ;; sign then digits
+      (:greedy-repetition 0 1 (:register (:char-class #\+ #\-)))
+      (:register (:greedy-repetition 0 nil (:char-class (:range #\0 #\9))))
+      ;; A decimal point
+      #\.
+      ;; maybe an exponent
+      (:greedy-repetition
+       0
+       1
+       (:group
+        (:sequence
+         (:register (:greedy-repetition 0 nil (:char-class (:range #\0 #\9))))
+         (:greedy-repetition
+          0
+          1
+          (:group
+           (:sequence
+            (:char-class #\e #\E)
+            (:greedy-repetition 0 1 (:register (:char-class #\+ #\-)))
+            (:register
+             (:greedy-repetition 0 nil (:char-class (:range #\0 #\9))))))))))
+      :end-anchor)
+     sign digits decimal-digits exponent-sign exponent-digits)
+  ;; Yet another cheapo PARSE-INTEGER
+  (flet ((->natural (s)
+           (etypecase s
+             (null 0)
+             (string
+              (with-accumulators ((r (lambda (c n)
+                                       (+ (* c 10) n))
+                                     0))
+                (dotimes (i (length s))
+                  (let ((d (digit-char-p (char s i) 10)))
+                    (unless d (error "bad digit string ~S" s))
+                    (r d)))))))
+         (->sign (s)
+           (etypecase s
+             (null 1)
+             (string
+              (cond
+               ((or (string= s "")
+                    (string= s "+"))
+                1)
+               ((string= s "-")
+                -1)
+               (t
+                (error "bad sign ~S" s)))))))
+    (* (->sign sign)
+       (+ (->natural digits)
+          (let ((decimal (->natural decimal-digits)))
+            (/ decimal (expt 10 (ceiling (log decimal 10))))))
+       (expt 10
+             (* (->sign exponent-sign)
+                (->natural exponent-digits))))))
--- token-parsers.lisp
+++ token-parsers.lisp
@@ -44,14 +44,40 @@
      (/ (decimal-string->natural numerator)
         (decimal-string->natural denominator))))
 
-;;; The zero-prioriry parser parses tokens as symbols which are just
-;;; interned into *PACKAGE* (there are no uninterned symbols)
+;;; The zero-priority parser parses tokens as symbols and now tries to
+;;; deal with packages (this is rather untested)
 ;;;
 
 (define-token-parser (symbol 0 :denatured t :reedtable *precursor-reedtable*)
     ((:sequence
-      :start-anchor
-      (:register (:greedy-repetition 0 nil :everything))
-      :end-anchor)
-     name)
-  (values (intern (string-upcase name) *package*)))
+     :start-anchor
+     ;; packege prefix perhaps
+     (:greedy-repetition
+      0
+      1
+      (:sequence
+       (:register (:greedy-repetition 0 nil (:inverted-char-class #\:)))
+       (:register (:greedy-repetition 1 2 (:char-class #\:)))))
+     ;; Symbol name
+     (:register (:greedy-repetition 0 nil (:inverted-char-class #\:)))
+     :end-anchor)
+     package-name colon/s name)
+  (let ((package (if colon/s
+                     (find-package
+                      (if (zerop (length package-name))
+                          "KEYWORD"
+                        (string-upcase package-name)))
+                   *package*)))
+    (unless package
+      (simple-reeder-error "no package ~S" package-name))
+    (if colon/s
+        (if (or (= (length colon/s) 2)
+                (eq package (find-package "KEYWORD")))
+            ;; Easy, just intern it
+            (values (intern (string-upcase name) package))
+          (multiple-value-bind (s status) (find-symbol (string-upcase name) package)
+            (unless (eq status :external)
+              (simple-reeder-error "~A is not external in ~A" s (package-name package)))
+            s))
+      ;; No colons
+      (values (intern (string-upcase name) package)))))
